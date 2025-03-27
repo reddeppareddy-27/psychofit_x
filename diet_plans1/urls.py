8cc1f8746b042bd3275148004f208278a171a3af
masireddyreddeppareddy <masireddyreddeppareddy@gmail.com>
2025-03-28 03:08:55 +0530

    first upload

--- diet_plans1/urls.py
+++ diet_plans1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from diet_plans.views import diet_plan_view
 
 urlpatterns = [
-    path('diet-plan/', diet_plan_view, name='diet_plan1'),
+    path('diet-plan1/', diet_plan_view, name='diet_plan1'),
 ]
